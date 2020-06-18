297c8b754fd375aa2ecff32550c4da98176bb8aa
Bùi Việt Dũng <dvbui@wisc.edu>
2020-06-18 00:32:25 -0400

    add index to synset

--- voc_hac.py
+++ voc_hac.py
@@ -1,14 +1,19 @@
 from nltk.corpus import wordnet as wn
 
 total_synset = {}
+index_to_synset = []
 
 
 def load_synset():
-    global total_synset
+    global total_synset, index_to_synset
     f = open("wordlist_formatted.txt", "r")
     for line in f:
         word = line.strip()
         synsets = wn.synsets(word)
         for synset in synsets:
             total_synset[synset] = {}
+
+    for synset in total_synset:
+        total_synset[synset] = len(index_to_synset)
+        index_to_synset.append(synset)
 
