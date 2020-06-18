ade040add32ef9884338733bf80e4dd40a605466
Bùi Việt Dũng <dvbui@wisc.edu>
2020-06-18 00:38:48 -0400

    compute distance matrix

--- voc_hac.py
+++ voc_hac.py
@@ -2,6 +2,7 @@ from nltk.corpus import wordnet as wn
 
 total_synset = {}
 index_to_synset = []
+distance_matrix = []
 
 
 def load_synset():
@@ -17,3 +18,17 @@ def load_synset():
         total_synset[synset] = len(index_to_synset)
         index_to_synset.append(synset)
 
+
+def compute_distance_matrix():
+    global distance_matrix
+    distance_matrix = [[0] * len(total_synset)] * len(total_synset)
+    for i in range(len(index_to_synset)):
+        for j in range(i+1, len(index_to_synset)):
+            similarity = index_to_synset[i].wup_similarity(index_to_synset[j])
+            if similarity is None:
+                similarity = 0
+            distance_matrix[i][j] = 1 - similarity
+
+
+load_synset()
+compute_distance_matrix()
